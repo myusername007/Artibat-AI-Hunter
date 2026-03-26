ad60f87a0f6b5be44a3de98d48d09d05706f40d2
myusername007 <nytka.roman@student.uzhnu.edu.ua>
2026-03-26 23:00:59 +0200

    fix(scheduler): update interval time

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,7 @@
 import logging
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from scrapers.leboncoin import scrape as leboncoin_scrape
+from scrapers.allovoisins import scrape as allovoisins_scrape
 
 logger = logging.getLogger("artibat.scheduler")
 
@@ -12,9 +13,17 @@ def setup_scheduler() -> AsyncIOScheduler:
     scheduler.add_job(
         leboncoin_scrape,
         "interval",
-        minutes=15,
+        minutes=27,
         id="leboncoin",
         name="Leboncoin scraper",
+    )
+
+    scheduler.add_job(
+        allovoisins_scrape,
+        "interval",
+        minutes=27,
+        id="allovoisins",
+        name="AlloVoisins scraper",
     )
 
     return scheduler
